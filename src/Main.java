e2f9be48c6cae13eeceecb4a8350601f2023a51f
HawkEyeVe <deutsch.escalante@gmail.com>
2025-05-30 13:44:07 -0400

    add txt file with 100 words and file reader functionality

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,27 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String word = "pizza";
+        String filepath = "src/words.txt";
+        ArrayList<String> words = new ArrayList<>();
+        String word;
+        try (BufferedReader reader = new BufferedReader(new FileReader(filepath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                words.add(line.trim().toLowerCase());
+            }
+            int random = (int) (Math.random() * words.size());
+            word = words.get(random);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
         Scanner sc = new Scanner(System.in);
         int guess = 0;
         ArrayList<Character> wordState = new ArrayList<>();
@@ -44,6 +62,7 @@ public class Main {
 
             if (guess == word.length() + 1) {
                 System.out.println("You lose!");
+                System.out.println("The word was: " + word);
                 break;
             }
         }
